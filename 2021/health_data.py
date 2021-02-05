bff8e77aef6b940bf2e538d53936795fbdae3ae0
portaljumper007 <enchantedportal007@gmail.com>
2021-02-05 16:54:11 +0000

    differential of weight

--- 2021/health_data.py
+++ 2021/health_data.py
@@ -8,6 +8,58 @@ from dateutil.relativedelta import relativedelta
 
 lines=[]
 
+def kernelRollingAverage(datapointsT, kernel=[[-604800,0,604800], [0,1,0]], certainties=[]): #datapointsT should already be filtered for None's
+    if len(certainties) == 0:
+        certainties = [1 for i in range(len(datapointsT[0]))]
+    d = datapointsT
+    out = []
+    termNum = (d[0][-1] - d[0][0]) / max(kernel[0])
+    pointsPerTerm = round(len(d[0]) / termNum)
+    
+    for p in range(len(d[0])):
+        window = [[], []]
+        timeDisps = []
+        for otherP in range(len(d[0])): #moving through the kernel window
+            timeDisp = (d[0][otherP] - d[0][p])
+            timeDisps.append(timeDisp)
+    
+        for n in range(len(timeDisps)):
+            if n > 0:
+                if timeDisps[n-1] <= 0 and timeDisps[n] >= 0:
+                    middleIndex = n
+        
+        win = [x for _,x in sorted(zip(np.abs(timeDisps),[p for p in range(len(timeDisps))]))][:pointsPerTerm-1]
+        winMult = np.abs(timeDisps[win[-1]]) / np.max(kernel[0])
+        for otherP in [x for _,x in sorted(zip(np.abs(timeDisps),[p for p in range(len(d[0]))]))][:pointsPerTerm-1]:
+            window[0].append(d[1][otherP]) #append the value to the window
+            window[1].append(np.interp([timeDisps[otherP]], np.array(kernel[0]) * winMult, kernel[1])[0] * certainties[otherP]) #append the weight to the window
+        out.append(np.average(window[0], weights=window[1]))
+    return out
+
+def drawMonths():
+    rang = [datapointsT[0][0], datapointsT[0][-1]]
+
+    strt_dt = datetime.fromtimestamp(rang[0])
+    end_dt = datetime.fromtimestamp(rang[1])
+
+    dates = [dt for dt in rrule(MONTHLY, dtstart=strt_dt, until=end_dt)]
+    dates.append(dates[-1] + relativedelta(months=1))
+    mTs = []
+    for month in dates:
+        month = month.replace(day=1)
+        month = month.replace(hour=0)
+        month = month.replace(minute=0)
+        month = month.replace(second=0)
+        mTs.append(month.timestamp())
+    for pair in range(len(mTs) - 1):
+        pairPerc = (pair) / len(mTs)
+        rect = patches.Rectangle((mTs[pair],0),mTs[pair+1]-mTs[pair],1,linewidth=1,facecolor=(0,pairPerc,1-pairPerc,0.15))
+        ax1.add_patch(rect)
+        ax1.text((mTs[pair] + mTs[pair+1]) / 2, 0, dates[pair].strftime('%B'), fontsize=12, horizontalalignment='center')
+    pairPerc = 1
+    rect = patches.Rectangle((mTs[-1],0),rang[1]-mTs[-1],1,linewidth=1,facecolor=(0,pairPerc,1-pairPerc,0.15))
+    ax1.add_patch(rect)
+
 #of running python script
 dirname, filename = os.path.split(os.path.abspath(__file__))
 
@@ -43,60 +95,21 @@ for line in lines:
 
 datapointsT = np.transpose(datapoints)
 
-def kernelRollingAverage(datapointsT, kernel=[[-604800,0,604800], [0,1,0]]): #datapointsT should already be filtered for None's
-    d = datapointsT
-    out = []
-    termNum = (d[0][-1] - d[0][0]) / max(kernel[0])
-    pointsPerTerm = round(len(d[0]) / termNum)
-    
-    for p in range(len(d[0])):
-        window = [[], []]
-        timeDisps = []
-        for otherP in range(len(d[0])): #moving through the kernel window
-            timeDisp = (d[0][otherP] - d[0][p])
-            timeDisps.append(timeDisp)
-    
-        for n in range(len(timeDisps)):
-            if n > 0:
-                if timeDisps[n-1] <= 0 and timeDisps[n] >= 0:
-                    middleIndex = n
-        
-        win = [x for _,x in sorted(zip(np.abs(timeDisps),[p for p in range(len(timeDisps))]))][:pointsPerTerm-1]
-        winMult = np.abs(timeDisps[win[-1]]) / np.max(kernel[0])
-        for otherP in [x for _,x in sorted(zip(np.abs(timeDisps),[p for p in range(len(d[0]))]))][:pointsPerTerm-1]:
-            window[0].append(d[1][otherP]) #append the value to the window
-            window[1].append(np.interp([timeDisps[otherP]], np.array(kernel[0]) * winMult, kernel[1])[0]) #append the weight to the window
-        out.append(np.average(window[0], weights=window[1]))
-    return out
+fig,ax = plt.subplots(2)
+ax1,ax2 = ax
 
-def drawMonths():
-    rang = [datapointsT[0][0], datapointsT[0][-1]]
+weight1stDifferential = [[], []]
+timeDiffs = np.diff(weight1stDifferential[0], n=1)
+for pair in range(len(datapointsT[0]) - 1):
+    weight1stDifferential[0].append((datapointsT[0][pair + 1] + datapointsT[0][pair]) / 2)
+    weight1stDifferential[1].append((datapointsT[1][pair + 1] - datapointsT[1][pair]) * (datapointsT[0][pair + 1] - datapointsT[0][pair]))
 
-    strt_dt = datetime.fromtimestamp(rang[0])
-    end_dt = datetime.fromtimestamp(rang[1])
-
-    dates = [dt for dt in rrule(MONTHLY, dtstart=strt_dt, until=end_dt)]
-    dates.append(dates[-1] + relativedelta(months=1))
-    mTs = []
-    for month in dates:
-        month = month.replace(day=1)
-        month = month.replace(hour=0)
-        month = month.replace(minute=0)
-        month = month.replace(second=0)
-        mTs.append(month.timestamp())
-    for pair in range(len(mTs) - 1):
-        pairPerc = (pair) / len(mTs)
-        rect = patches.Rectangle((mTs[pair],0),mTs[pair+1]-mTs[pair],1,linewidth=1,facecolor=(0,pairPerc,1-pairPerc,0.15))
-        ax.add_patch(rect)
-        plt.text((mTs[pair] + mTs[pair+1]) / 2, 0, dates[pair].strftime('%B'), fontsize=12, horizontalalignment='center')
-    pairPerc = 1
-    rect = patches.Rectangle((mTs[-1],0),rang[1]-mTs[-1],1,linewidth=1,facecolor=(0,pairPerc,1-pairPerc,0.15))
-    ax.add_patch(rect)
-
-fig,ax = plt.subplots(1)
+term = 604800 * 2 #in seconds, for moving average
+x,y = weight1stDifferential[0], kernelRollingAverage(weight1stDifferential, kernel=[[-term,0,term], [0,1,0]], certainties = timeDiffs)
+ax2.plot(x, y, linewidth=0.75, alpha=1, label="weight 1st differential")
 
 allOnTheSameGraph = True
-term = 604800 * 4 #in seconds, for moving average
+term = 604800 * 2 #in seconds, for moving average
 
 if allOnTheSameGraph:
     rang = 1
@@ -125,7 +138,7 @@ for i in range(rang):
 
         y = kernelRollingAverage([x,y], [[-term,0,term], [0,1,0]])
 
-        plt.plot(x, y, linewidth=0.75, alpha=1, label=fieldNames[fieldNum])
+        ax1.plot(x, y, linewidth=0.75, alpha=1, label=fieldNames[fieldNum])
         plt.legend()
         if not allOnTheSameGraph:
             drawMonths()
