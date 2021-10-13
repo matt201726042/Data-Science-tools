39c55e805718b484c1965d2f158a1f40dd7dbf52
portaljumper007 <enchantedportal007@gmail.com>
2021-10-14 00:51:50 +0100

    testing

--- quant/newForecaster.py
+++ quant/newForecaster.py
@@ -1,108 +1,106 @@
-from scipy import signal
-from scipy.spatial.transform import Rotation as R
-import numpy as np
-from scipy.interpolate import PchipInterpolator
-import numpy as np
-import vispy
-from vispy import app, visuals, scene
-from vispy.scene import visuals
-from vispy import app
-from vispy.util.quaternion import Quaternion
-import sys
-import random
-import math
-import bisect
-import time
+def multiprocessing_func(Global, id, t):
+    from scipy import signal
+    from scipy.spatial.transform import Rotation as R
+    import numpy as np
+    from scipy.interpolate import PchipInterpolator
+    import numpy as np
+    import vispy
+    from vispy import app, visuals, scene
+    from vispy.scene import visuals
+    from vispy import app
+    from vispy.util.quaternion import Quaternion
+    import sys
+    import random
+    import math
+    import bisect
+    import time
 
-import stocks as s
+    import stocks as s
 
-import multiprocessing
-from multiprocessing import Process
-
-def ndEvenDist(ndBounds, count):
+    import multiprocessing
+    from multiprocessing import Process
+    def ndEvenDist(ndBounds, count):
         z = np.array([[np.linspace(ndBounds[dim][param][0].astype(np.float64), ndBounds[dim][param][1].astype(np.float64), num=math.ceil(count ** (1 / np.size(ndBounds, axis=0))), endpoint=True, retstep=False, dtype=None, axis=0) for param in range(np.size(ndBounds[dim], axis=0))] for dim in range(np.size(ndBounds, axis=0))])
         z = z.reshape(-1, z.shape[-1])
         meshgrid = np.meshgrid(*z)
         meshgrid = [meshgrid[i].flatten() for i in range(np.size(meshgrid, axis=0))]
         return [meshgrid[a][list(set(np.floor(np.linspace(0, len(meshgrid[a]), num=count + 2, endpoint=True, retstep=False, dtype=None, axis=0)[1:-1]).astype(int)))] for a in range(len(meshgrid))]
+    def forecaster(DATA, signalSamples, phaseSamples, binCount):
+        DATA = np.array(DATA).astype(np.float64)
+        DATAdims = len(DATA)
 
-def forecaster(DATA, signalSamples, phaseSamples, binCount):
-    DATA = np.array(DATA).astype(np.float64)
-    DATAdims = len(DATA)
+        ndBounds = []
+        pchips = []
+        binBounds = []
+        bins = []
+        for dim in range(np.size(DATA, axis=0)):
+            dimRange = np.amax(DATA[dim]) - np.amin(DATA[dim])
+            ndBounds.append([[-dimRange, dimRange]])
+            if dim > 0:
+                pchips.append(PchipInterpolator(DATA[0], DATA[dim]))
+            else:
+                binBounds = np.linspace(DATA[0][0] - dimRange, dimRange + DATA[0][-1], binCount+1, endpoint=True)
+                bins = [[] for i in range(len(binBounds)-1)]
+        sims = ndEvenDist(ndBounds, phaseSamples)
+        
+        result = {"data":[], "weights":[], "lens":[]}
+        simCount = np.size(sims[0])
+        startTime = time.perf_counter()
 
-    ndBounds = []
-    pchips = []
-    binBounds = []
-    bins = []
-    for dim in range(np.size(DATA, axis=0)):
-        dimRange = np.amax(DATA[dim]) - np.amin(DATA[dim])
-        ndBounds.append([[-dimRange, dimRange]])
-        if dim > 0:
-            pchips.append(PchipInterpolator(DATA[0], DATA[dim]))
-        else:
-            binBounds = np.linspace(DATA[0][0] - dimRange, dimRange + DATA[0][-1], binCount+1, endpoint=True)
-            bins = [[] for i in range(len(binBounds)-1)]
-    sims = ndEvenDist(ndBounds, phaseSamples)
-    
-    result = {"data":[], "weights":[], "lens":[]}
-    simCount = np.size(sims[0])
-    startTime = time.perf_counter()
+        for s in range(simCount):
+            expTimeTaken = (simCount / (s+1)) * (time.perf_counter() - startTime)
+            if expTimeTaken > 1 and (s+1) % int(((1/expTimeTaken)*simCount)) == 0:
+                print(str(int(((s+1)/simCount)*100)) + "%", str(int(1/expTimeTaken)) + "fps", str(np.round(expTimeTaken - (time.perf_counter() - startTime), 2)) + "s remain")
+            tempDATA = DATA.copy()
+            simParams = [sims[0][s], sims[1][s]]
+            for d in range(len(simParams)):
+                tempDATA[d] += simParams[d]
+            result["data"].append(tempDATA)
 
-    for s in range(simCount):
-        expTimeTaken = (simCount / (s+1)) * (time.perf_counter() - startTime)
-        if expTimeTaken > 1 and (s+1) % int(((1/expTimeTaken)*simCount)) == 0:
-            print(str(int(((s+1)/simCount)*100)) + "%", str(int(1/expTimeTaken)) + "fps", str(np.round(expTimeTaken - (time.perf_counter() - startTime), 2)) + "s remain")
-        tempDATA = DATA.copy()
-        simParams = [sims[0][s], sims[1][s]]
-        for d in range(len(simParams)):
-            tempDATA[d] += simParams[d]
-        result["data"].append(tempDATA)
-
-        tempDists = []
-        for i in range(len(tempDATA[0])):
-            if not(bisect.bisect_right(DATA[0], tempDATA[0][i]) == 0 or bisect.bisect_left(DATA[0], tempDATA[0][i]) == len(DATA[0])):
-                dimDists = []
-                for d in range(1, len(simParams)):
-                    dimDists.append(abs(pchips[d-1](tempDATA[0][i]) - tempDATA[d][i]) ** 2)
-                dDist = (sum(dimDists) ** 0.5)
-                tempDists.append(dDist)
-        tempDistsMean = np.mean(tempDists)
-        lenPerc = len(tempDists)/(len(DATA[0]))
-        for i in range(len(tempDATA[0])):
-            b = bisect.bisect(binBounds, tempDATA[0][i])
-            if len(tempDists) != 0 and not(b == 0 or b == binCount+1):
-                bins[b-1].append([[tempDATA[a][i] for a in range(1,DATAdims)], (((1-tempDistsMean) + lenPerc)/2) + 1])
-        result["weights"].append(tempDistsMean)
-        result["lens"].append(lenPerc)
-    result["binned"] = [[] for d in range(DATAdims-1)]
-    result["binLocs"] = [np.mean([binBounds[i], binBounds[i+1]]) for i in range(binCount)]
-    for b in range(len(bins)):
-        count = [0 for i in range(DATAdims)]
-        total = [0 for i in range(DATAdims)]
-        #bestWeight = 0
-        #bestValue = [0 for i in range(DATAdims)]
-        for i in range(len(bins[b])):
+            tempDists = []
+            for i in range(len(tempDATA[0])):
+                if not(bisect.bisect_right(DATA[0], tempDATA[0][i]) == 0 or bisect.bisect_left(DATA[0], tempDATA[0][i]) == len(DATA[0])):
+                    dimDists = []
+                    for d in range(1, len(simParams)):
+                        dimDists.append(abs(pchips[d-1](tempDATA[0][i]) - tempDATA[d][i]) ** 2)
+                    dDist = (sum(dimDists) ** 0.5)
+                    tempDists.append(dDist)
+            tempDistsMean = np.mean(tempDists)
+            lenPerc = len(tempDists)/(len(DATA[0]))
+            for i in range(len(tempDATA[0])):
+                b = bisect.bisect(binBounds, tempDATA[0][i])
+                if len(tempDists) != 0 and not(b == 0 or b == binCount+1):
+                    bins[b-1].append([[tempDATA[a][i] for a in range(1,DATAdims)], (((1-tempDistsMean) + lenPerc)/2) + 1])
+            result["weights"].append(tempDistsMean)
+            result["lens"].append(lenPerc)
+        result["binned"] = [[] for d in range(DATAdims-1)]
+        result["binLocs"] = [np.mean([binBounds[i], binBounds[i+1]]) for i in range(binCount)]
+        for b in range(len(bins)):
+            count = [0 for i in range(DATAdims)]
+            total = [0 for i in range(DATAdims)]
+            #bestWeight = 0
+            #bestValue = [0 for i in range(DATAdims)]
+            for i in range(len(bins[b])):
+                for d in range(DATAdims-1):
+                    total[d] += bins[b][i][0][d] * (bins[b][i][1]**100)
+                    count[d] += (bins[b][i][1]**100)
+                #if bins[b][i][1] > bestWeight:
+                    #bestWeight = bins[b][i][1]
+                    #bestValue = bins[b][i][0]
             for d in range(DATAdims-1):
-                total[d] += bins[b][i][0][d] * (bins[b][i][1]**100)
-                count[d] += (bins[b][i][1]**100)
-            #if bins[b][i][1] > bestWeight:
-                #bestWeight = bins[b][i][1]
-                #bestValue = bins[b][i][0]
-        for d in range(DATAdims-1):
-            try:
-                result["binned"][d].append(total[d] / count[d])
-                #result["binned"][d].append(bestValue[d])
-            except:
-                result["binned"][d].append(0)
-    result["lens"] = np.array(result["lens"]).astype(np.float64)
-    result["weights"] = np.array(result["weights"])
-    result["weights"] -= np.amin(result["weights"])
-    result["weights"] /= np.amax(result["weights"])
-    result["weights"] = ((1-result["weights"]) + result["lens"]) / 2 #1 - (((1 - ((1 - result["weights"]) ** 1)) + (1-result["lens"])) / 2)
-    return result
-
-def multiprocessing_func(id, t):
+                try:
+                    result["binned"][d].append(total[d] / count[d])
+                    #result["binned"][d].append(bestValue[d])
+                except:
+                    result["binned"][d].append(0)
+        result["lens"] = np.array(result["lens"]).astype(np.float64)
+        result["weights"] = np.array(result["weights"])
+        result["weights"] -= np.amin(result["weights"])
+        result["weights"] /= np.amax(result["weights"])
+        result["weights"] = ((1-result["weights"]) + result["lens"]) / 2 #1 - (((1 - ((1 - result["weights"]) ** 1)) + (1-result["lens"])) / 2)
+        return result
     while True:
+        startTime = time.perf_counter()
         t += 0.1
         #DATA = [np.linspace(0,100, np.size(stockData)), stockData]
         #DATA[0].append(t+0.1)
@@ -129,12 +127,33 @@ def multiprocessing_func(id, t):
         
         #out = Global.out
         out = {"DATA":DATA, "binData":binData, "colours":colours, "outData":outData, "t":t}
+        print("B", 1 / (time.perf_counter() - startTime))
         Global.out = out
     #Global.alive = False
 
 if __name__ == '__main__':
+    from scipy import signal
+    from scipy.spatial.transform import Rotation as R
+    import numpy as np
+    from scipy.interpolate import PchipInterpolator
+    import numpy as np
+    import vispy
+    from vispy import app, visuals, scene
+    from vispy.scene import visuals
+    from vispy import app
+    from vispy.util.quaternion import Quaternion
+    import sys
+    import random
+    import math
+    import bisect
+    import time
+
+    import stocks as s
+
+    import multiprocessing
+    from multiprocessing import Process
     manager = multiprocessing.Manager()
-    global Global
+    #global Global
     global processes
     Global = manager.Namespace()
     processes = []
@@ -165,16 +184,16 @@ if __name__ == '__main__':
     axis = visuals.XYZAxis(parent=view.scene)
 
     t = -0.1
+    Global.out = {"DATA":[], "binData":[], "colours":[], "outData":[], "t":t}
     #DATA = [[0], [0]]
     #stockData = s.getStockData()[:3000]
-    Global.out = {"DATA":[], "binData":[], "colours":[], "outData":[], "t":t}
 
     def main():
         print("main: started")
         coreCount = 1#int(multiprocessing.cpu_count() * 0.5)
         processes = []
         for id in range(coreCount):
-            p = multiprocessing.Process(target=multiprocessing_func, args=(id, t))
+            p = multiprocessing.Process(target=multiprocessing_func, args=(Global, id, t))
             p.daemon = True
             processes.append(p)
         for core in range(coreCount):
@@ -198,7 +217,7 @@ if __name__ == '__main__':
             colours = Global.out["colours"]
             outData = Global.out["outData"]
             t = Global.out["t"]
-            print(1 / (time.perf_counter() - startTime))
+            print("A", 1 / (time.perf_counter() - startTime))
             scatter.set_data(outData[:100000], edge_color=(0,0,1,0), face_color=colours[:100000], size=3)
             #scatterBase.set_data(pos=self.out[0], connect=self.out[1], width=1000, color=(0.5, 0.5, 1, 1))
             scatterBase.set_data(np.transpose(DATA + np.zeros(np.size(DATA, 1))), connect=np.array([[i, i+1] for i in range(len(DATA[0]) - 1)]), color=(1, 1, 1, 1), edge_color=(0.5, 0.5, 1, 0), width=4, face_color=(0.5, 0.5, 1, 0))
--- quant/threadstest.py
+++ quant/threadstest.py
@@ -1,6 +1,11 @@
+import time
 def multiprocessing_func(Global, id):
+    i = 0
     while True:
-        print("a",id)
+        i += 1.1
+        for j in range(10000000):
+            i += 0.00000001
+        Global.out = i
     out = Global.out
     out.append(id)
     Global.out = out
@@ -13,19 +18,16 @@ if __name__ == '__main__':
     global Global
     global processes
     Global = manager.Namespace()
-    Global.out = []
+    Global.out = 0
     processes = []
 
-    def main():
-        coreCount = 1#int(multiprocessing.cpu_count() * 0.5)
-        for id in range(coreCount):
-            p = multiprocessing.Process(target=multiprocessing_func, args=(Global, id))
-            p.daemon = True
-            processes.append(p)
-        for core in range(coreCount):
-            processes[core].start()
-        #processes[-1].join()
-    main()
+    coreCount = 1#int(multiprocessing.cpu_count() * 0.5)
+    for id in range(coreCount):
+        p = multiprocessing.Process(target=multiprocessing_func, args=(Global, id))
+        p.daemon = True
+        processes.append(p)
+    for core in range(coreCount):
+        processes[core].start()
+    #processes[-1].join()
     while True:
-        print("b",0)
-    print(Global.out)
+        print("b",Global.out)
